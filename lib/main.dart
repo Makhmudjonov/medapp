ccca253387c17d05c245ccbd3b0a7730fd613095
Makhmudjonov <makhmudjonov14@gmail.com>
2025-02-18 06:46:23 +0500

    l

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:medapp/screen/first_login.dart';
+import 'package:medapp/screen/home.dart';
 import 'package:medapp/screen/login.dart';
 import 'package:medapp/screen/splash_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  String? token = await getToken();
+  runApp(MyApp(
+    isLoggedIn: token != null,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  MyApp({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +25,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  LoginPage(),
+      home: isLoggedIn ? HomePage() : LoginPage(),
     );
   }
+}
+
+Future<String?> getToken() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString("token");
 }
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,5 +1,7 @@
-// 🔑 LOGIN PAGE
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:medapp/screen/home.dart';
 import 'package:medapp/screen/signup.dart';
 
@@ -11,8 +13,9 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
 
-  void login() {
+  Future<void> login() async {
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
@@ -23,54 +26,127 @@ class _LoginPageState extends State<LoginPage> {
       return;
     }
 
-    // Bu yerda autentifikatsiya kodini qo'shishingiz mumkin
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => HomePage()),
-    );
+    setState(() {
+      isLoading = true;
+    });
+
+    final Uri url = Uri.parse("http://13.49.49.224:8080/api/auth/login");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({
+          "email": email,
+          "password": password,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = response.body.toString();
+        print(data.toString());
+        String token = data.toString(); // API token qaytarishi kerak
+
+        // Tokenni localda saqlash
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString("token", token);
+
+        // Muvaffaqiyatli login bo‘lsa, HomePage-ga o‘tish
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Login xatosi: ${response.body}")),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Xatolik: $e")),
+      );
+    }
+
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Padding(
-            padding: EdgeInsets.all(40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Login here",style: TextStyle(
-                      color: Color(0xFF1F41BB),
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,),
-                Text("Welcome back you've been missed!",style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 26,
-                      // fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.061),
-                TextField(  keyboardType: TextInputType.emailAddress,
-decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),hintText: "E-mail",labelText: "E-mail",),),
-                SizedBox(height: 10,),
-                TextField(obscureText: true,decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),hintText: "Password",labelText: "Password"),),
-                SizedBox(height: 10,),
-                Row(mainAxisAlignment: MainAxisAlignment.end,
-                  children: [Text("Forget your password?")],
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login here",
+                style: TextStyle(
+                  color: Color(0xFF1F41BB),
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
                 ),
-                SizedBox(height: 10,),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  width: 150,
-                  color: Color(0xFF1F41BB)
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Welcome back you've been missed!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
                 ),
-                SizedBox(height: 10,),
-                Text("Create new account"),
-              ],
-            ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.061),
+              TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  hintText: "E-mail",
+                  labelText: "E-mail",
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  hintText: "Password",
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [Text("Forget your password?")],
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: isLoading ? null : login,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF1F41BB),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                child: isLoading
+                    ? CircularProgressIndicator(color: Colors.white)
+                    : Text("Login", style: TextStyle(fontSize: 18)),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                },
+                child: Text("Create new account"),
+              ),
+            ],
           ),
         ),
       ),
